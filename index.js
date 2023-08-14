c44725c1c64acb7a5fd504190dcfd5d86546d751
Keaton <keaton.yoputra@gmail.com>
2023-08-15 01:08:31 +0700

    Commit part3 rev7

--- index.js
+++ index.js
@@ -100,9 +100,10 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
+  console.log(error)
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" })
-  } else if (error.name === "ValidationError" || error.number === "ValidationError") {
+  } else if (error.name === "ValidationError") {
     return response.status(400).json({ error: error.message })
   }
   next(error)
